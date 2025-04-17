e17638bde027e9be5f41039b6624f8d19aff9043
Akwaji Dan Uke <danakwaji3@gmail.com>
2025-04-17 12:05:42 +0100

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { ToastContainer } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 
 const App = () => {
   return (
